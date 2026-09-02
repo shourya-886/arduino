a91d39e9fad72e6aef9a5d322031de4bb0082914
shourya <pihushourya100@gmail.com>
2026-09-02 18:18:54 +0530

    added support for control

--- src/arduinobot_description/launch/gazebo.launch.py
+++ src/arduinobot_description/launch/gazebo.launch.py
@@ -28,9 +28,16 @@ def generate_launch_description():
         )
     
     ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -43,7 +50,7 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
                 launch_arguments=[
-                    ("gz_args", [" -v 4 -r empty.sdf "]
+                    ("gz_args", [" -v 4 -r empty.sdf"]
                     )
                 ]
              )
