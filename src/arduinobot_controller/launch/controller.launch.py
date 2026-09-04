d3f1013d1f7cc729d41e5076724ff6f124b3ddf0
shourya <pihushourya100@gmail.com>
2026-09-04 09:49:46 +0530

    added firmware and examples of lifecycle nodes, updated cmake and package.xml,

--- src/arduinobot_controller/launch/controller.launch.py
+++ src/arduinobot_controller/launch/controller.launch.py
@@ -1,22 +1,21 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import UnlessCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
+from launch.conditions import UnlessCondition
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
+    is_sim = LaunchConfiguration("is_sim")
+    
     is_sim_arg = DeclareLaunchArgument(
         "is_sim",
         default_value="True"
     )
-
-    is_sim = LaunchConfiguration("is_sim")
 
     robot_description = ParameterValue(
         Command(
@@ -27,6 +26,7 @@ def generate_launch_description():
                     "urdf",
                     "arduinobot.urdf.xacro",
                 ),
+                " is_sim:=False"
             ]
         ),
         value_type=str,
@@ -35,8 +35,9 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": False}],
         condition=UnlessCondition(is_sim),
-        parameters=[{"robot_description": robot_description}],
     )
 
     controller_manager = Node(
